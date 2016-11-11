98de0a807dd6ab499efa12c10258bc6beb24cec0
justo <justojul23@hotmail.com>
2016-11-11 02:00:03 -0600

    funciones mayor,menor,suma,resta,promedio

--- src/jexcelcompi/JExcelCompi.java
+++ src/jexcelcompi/JExcelCompi.java
@@ -29,27 +29,114 @@ public class JExcelCompi {
      * @param args the command line arguments
      */
     public  static void main(String[] args) {     
-        String nombre;
-        nombre = "hoja2.xls";
-         celdaUnitaria(nombre,4,2);
+        String nombre="hoja.xls,hoja2.xls,hoja3.xls";
+        System.out.println("resultado "+menor(nombre, 4, 2));
+        
+        
+        
+         
         // TODO code application logic here
     }
     
     //extrae valor numerico de celdas del tipo hoja1[fila,columna]
-     static void celdaUnitaria(String nombre,int fila,int columna)
+     static int celdaUnitaria(String nombre,int fila,int columna)
     {
+        int valor=0;
         try {
             FileInputStream fis = new FileInputStream(new File(nombre));
             HSSFWorkbook wb = new HSSFWorkbook(fis);
             HSSFSheet sheet = wb.getSheetAt(0); 
-            System.out.println(wb.getSheetAt(0).getRow(fila).getCell(columna));
+            System.out.println("dato "+wb.getSheetAt(0).getRow(fila).getCell(columna));
+            valor = (int)(double)(wb.getSheetAt(0).getRow(fila).getCell(columna).getNumericCellValue());
+        } catch (IOException ex) {
+            Logger.getLogger(JExcelCompi.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return valor;
+    }
+     
+     /*saca el valor de la celda especifica que se encuentra en la hoja numHoja*/
+     static void HojaNceldaUnitaria(String nombre,int numHoja,int fila,int columna)
+    {
+        try {
+            FileInputStream fis = new FileInputStream(new File(nombre));
+            HSSFWorkbook wb = new HSSFWorkbook(fis);
+            HSSFSheet sheet = wb.getSheetAt(numHoja); 
+            System.out.println(wb.getSheetAt(numHoja).getRow(fila).getCell(columna));
             
         } catch (IOException ex) {
             Logger.getLogger(JExcelCompi.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
+     
+     static double suma(String nombre,int fila,int columna)
+     {
+         String nombres[] = nombre.split(",");
+        int totalSuma=0;
+        
+        for(int i =0 ; i < nombres.length ;i++)
+            {
+                totalSuma = totalSuma + celdaUnitaria(nombres[i], fila, columna);
+            }       
+         
+         return totalSuma;
+     }
+     
+     static double resta(String nombre,int fila,int columna)
+     {
+         String nombres[] = nombre.split(",");
+        int totalResta=0;
+        totalResta = celdaUnitaria(nombres[0], fila, columna);
+        for(int i =1 ; i < nombres.length ;i++)
+            {
+                totalResta = totalResta - celdaUnitaria(nombres[i], fila, columna);
+            }       
+         
+         return totalResta;
+     }
+     
+    static double promedio(String nombre,int fila,int columna)
+     {
+         String nombres[] = nombre.split(",");
+        int total=0;
+        int i =0;
+        for(i=0 ; i < nombres.length ;i++)
+            {
+                total = total + celdaUnitaria(nombres[i], fila, columna);
+            }       
+         total = total/i;
+         return total;
+     }
     
+    static double mayor(String nombre,int fila,int columna)
+     {
+         String nombres[] = nombre.split(",");
+        int mayor=0;
+        int i =0;
+        for(i=0 ; i < nombres.length ;i++)
+            {
+                if(celdaUnitaria(nombres[i], fila, columna)>mayor)
+                    mayor = celdaUnitaria(nombres[i], fila, columna);
+                
+            }       
+         
+         return mayor;
+     }
+    
+    static double menor(String nombre,int fila,int columna)
+     {
+         String nombres[] = nombre.split(",");
+        int menor=celdaUnitaria(nombres[0], fila, columna);
+        int i =0;
+        for(i=0 ; i < nombres.length ;i++)
+            {
+                if(celdaUnitaria(nombres[i], fila, columna)<menor)
+                    menor = celdaUnitaria(nombres[i], fila, columna);
+                
+            }       
+         
+         return menor;
+     }
     
     public void ejemplo()throws FileNotFoundException, IOException 
     {
